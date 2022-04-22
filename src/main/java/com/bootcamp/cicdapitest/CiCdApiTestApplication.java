09dc151f5c73d0697ca1cc08385a0a404310fa8d
samuel14luis <luismendoza.samuel@gmail.com>
2022-04-22 12:04:57 -0500

    scaffolding

---
+++ src/main/java/com/bootcamp/cicdapitest/CiCdApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.cicdapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdApiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/cicdapitest/CiCdApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.cicdapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
